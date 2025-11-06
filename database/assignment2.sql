6159d79d147f3ef82e98cff4bb396cd14a94cad3
Denton Flake <me@dentonflake.com>
2025-11-05 19:12:05 -0700

    Added sql scripts

---
+++ database/assignment2.sql
@@ -0,0 +1,45 @@
+-- 1
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+  )
+VALUES (
+    'Tony',
+    'Stark',
+    'tony@starkent.com',
+    'Iam1ronM@n'
+  )
+  
+-- 2
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = 1
+
+-- 3
+DELETE FROM public.account
+WHERE account_id = 1
+
+-- 4
+UPDATE public.inventory
+SET inv_description = REPLACE(
+    inv_description,
+    'small interiors',
+    'huge interiors'
+)
+WHERE inv_id = 10
+
+-- 5
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM public.inventory i
+JOIN public.classification c ON i.classification_id = c.classification_id
+WHERE c.classification_id = 2
+
+-- 6
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+WHERE TRUE
+    
