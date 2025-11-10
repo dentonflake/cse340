c25afb1712ea2766efa2b411e31b4d1c1cd00900
Denton Flake <me@dentonflake.com>
2025-11-10 14:22:59 -0700

    Completed w03 assignment

---
+++ controllers/invController.js
@@ -0,0 +1,35 @@
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
+
+const invCont = {}
+
+/* ***************************
+ *  Build inventory by classification view
+ * ************************** */
+invCont.buildByClassificationId = async function (req, res, next) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
+}
+
+invCont.buildByInventoryId = async function (req, res, next) {
+  const inventory_id = req.params.inventoryId
+  const data = await invModel.getItemByInventoryId(inventory_id)
+  const details = await utilities.buildItemDetails(data)
+  let nav = await utilities.getNav()
+  const title = data.inv_year + ' ' + data.inv_make + ' ' + data.inv_model
+  res.render("./inventory/details", {
+    title,
+    nav,
+    details
+  })
+}
+
+module.exports = invCont
